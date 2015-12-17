e07d404a03b7c3b27bd0ea9f613131dc19916721
Ildar Isaev <ild@inbox.ru>
2015-12-17 16:04:53 +0200

    further defs and refs support in java script

--- toolchain-js/src/main/java/com/sourcegraph/toolchain/js/JavaScriptParseTreeListener.java
+++ toolchain-js/src/main/java/com/sourcegraph/toolchain/js/JavaScriptParseTreeListener.java
@@ -23,8 +23,12 @@ class JavaScriptParseTreeListener extends JavaScriptBaseListener {
 
     private LanguageImpl support;
 
+    private Context ctxt;
+
     public JavaScriptParseTreeListener(LanguageImpl support) {
         this.support = support;
+        this.ctxt = new Context();
+        ctxt.pushScope(); //global scope
     }
 
     @Override
@@ -32,10 +36,74 @@ class JavaScriptParseTreeListener extends JavaScriptBaseListener {
         Def funcDef = support.def(ctx.Identifier().getSymbol(), "function");
         funcDef.defKey = new DefKey(null, funcDef.name);
         support.emit(funcDef);
+        ctxt.addToCurrentScope(funcDef);
+        ctxt.pushScope();
+    }
+
+    @Override
+    public void enterFormalParameterList(JavaScriptParser.FormalParameterListContext ctx) {
+        List<TerminalNode> params = ctx.Identifier();
+        for (TerminalNode param : params) {
+            Def paramDef = support.def(param.getSymbol(), "param");
+            paramDef.defKey = new DefKey(null, paramDef.name);
+            support.emit(paramDef);
+            ctxt.addToCurrentScope(paramDef);
+        }
+    }
+
+    @Override
+    public void enterVariableDeclaration(JavaScriptParser.VariableDeclarationContext ctx) {
+        Def varDef = support.def(ctx.Identifier().getSymbol(), "var");
+        varDef.defKey = new DefKey(null, varDef.name);
+        support.emit(varDef);
+        ctxt.addToCurrentScope(varDef);
+    }
+
+    @Override
+    public void enterIdentifierExpression(JavaScriptParser.IdentifierExpressionContext ctx) {
+        Def d = ctxt.find(ctx.Identifier().getSymbol().getText());
+        if (d != null) {
+            Ref ref = support.ref(ctx.Identifier().getSymbol());
+            ref.defKey = d.defKey;
+            support.emit(ref);
+        }
+    }
+
+    @Override
+    public void enterFunctionExpression(JavaScriptParser.FunctionExpressionContext ctx) {
+        if (ctx.Identifier() != null) {
+            Def funcDef = support.def(ctx.Identifier().getSymbol(), "function");
+            funcDef.defKey = new DefKey(null, funcDef.name);
+            support.emit(funcDef);
+            //should we do this?
+            //ctxt.addToCurrentScope(funcDef);
+        }
+        ctxt.pushScope();
+    }
+
+    @Override
+    public void enterBlock(JavaScriptParser.BlockContext ctx) {
+        ctxt.pushScope();
+    }
+
+    @Override
+    public void exitBlock(JavaScriptParser.BlockContext ctx) {
+        ctxt.pushScope();
+    }
+
+    @Override
+    public void exitFunctionExpression(JavaScriptParser.FunctionExpressionContext ctx) {
+        ctxt.popScope();
+    }
+
+    @Override
+    public void exitFunctionDeclaration(JavaScriptParser.FunctionDeclarationContext ctx) {
+        ctxt.popScope();
     }
 
     @Override
     public void enterProgram(JavaScriptParser.ProgramContext ctx) {
+        //support.emit();
     }
 
 

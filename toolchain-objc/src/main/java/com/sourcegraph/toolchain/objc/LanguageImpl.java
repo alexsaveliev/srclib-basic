cf1da2ee7d12e50e9bbf8f350b8b418dbf39475f
Alex Saveliev <lyolik@codeminders.com>
2016-01-13 11:04:08 +0200

    code cleanup

--- toolchain-objc/src/main/java/com/sourcegraph/toolchain/objc/LanguageImpl.java
+++ toolchain-objc/src/main/java/com/sourcegraph/toolchain/objc/LanguageImpl.java
@@ -1,14 +1,11 @@
 package com.sourcegraph.toolchain.objc;
 
-import com.sourcegraph.toolchain.core.GraphWriter;
 import com.sourcegraph.toolchain.core.objects.DefKey;
 import com.sourcegraph.toolchain.language.*;
 import com.sourcegraph.toolchain.objc.antlr4.ObjCLexer;
 import com.sourcegraph.toolchain.objc.antlr4.ObjCParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.HashMap;
@@ -18,19 +15,12 @@ import java.util.Set;
 
 public class LanguageImpl extends LanguageBase {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LanguageImpl.class);
-
-    GraphWriter writer;
-
     Map<String, String> globalVars = new HashMap<>();
     // class name -> (variable -> type)
     Map<String, Map<String, String>> instanceVars = new HashMap<>();
 
     Set<String> functions = new HashSet<>();
     Set<String> types = new HashSet<>();
-
-    private Set<String> visited = new HashSet<>();
-    private Set<String> files;
 
     @Override
     protected void parse(File sourceFile) throws ParseException {
